44f8b9710c60d0ae984b556669a2d50a8e7d9a0c
Cheșa David <140747134+ChesaDavid@users.noreply.github.com>
2024-01-05 17:46:31 +0200

    New commit

--- game.py
+++ game.py
@@ -1,7 +1,13 @@
 
 from turtle import *
+from keyboard import *
 score = 0
 highscore = 0
+
+add_hotkey("left",lambda:Left())
+add_hotkey("right",lambda:Right())
+add_hotkey("up",lambda:Up())
+add_hotkey("down",lambda:Down())
 
 screen = Screen()
 screenMinx = -screen.window_width()/2
@@ -12,4 +18,12 @@ screenMaxY = screen.window_height()/2
 player = Turtle()
 player.goto(0, 0)
 player.speed(10)
+
+def Right():
+    while is_pressed("left"):
+        
+def Left():
+def Up():
+def Down():
+
 screen.mainloop()

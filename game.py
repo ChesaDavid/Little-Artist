fb5ce6fa981d3a5dd9bb03a3d547214a6a31a76d
Cheșa David <140747134+ChesaDavid@users.noreply.github.com>
2024-01-06 13:05:31 +0200

    New commit

--- game.py
+++ game.py
@@ -1,29 +1,82 @@
+import turtle
+import random
+import math
 
-from turtle import *
-from keyboard import *
-score = 0
-highscore = 0
+screen = turtle.Screen()
+screen.title("My game by python code")
+screen.bgcolor("black")
+screen.setup(width=600, height=600)
 
-add_hotkey("left",lambda:Left())
-add_hotkey("right",lambda:Right())
-add_hotkey("up",lambda:Up())
-add_hotkey("down",lambda:Down())
+# Making the user 'bubble'
+bubble = turtle.Turtle()
+bubble.color("red")
+bubble.shape("circle")
+bubble.penup()
+speed = 3
 
-screen = Screen()
-screenMinx = -screen.window_width()/2
-screenMiny = -screen.window_height()/2
-screenMaxX = screen.window_width()/2
-screenMaxY = screen.window_height()/2
+# Making the collection balls
+collection_ball = turtle.Turtle()
+collection_ball.color("red")
+collection_ball.penup()
+collection_ball.shape("circle")
+collection_ball.shapesize(0.5, 0.5, 0.5)
+ball_cor1 = random.randint(30, 280)
+ball_cor2 = random.randint(30, 280)
+collection_ball.setposition(ball_cor1, ball_cor2)
+collection_ball.color("yellow")
 
-player = Turtle()
-player.goto(0, 0)
-player.speed(10)
+# Scoring
+points = turtle.Turtle()
+points.color("yellow")
+style = ('Courier', 30, 'italic')
+points.penup()
+points.goto(-200, 250)
+points.write("Points: 0", font=style)
+points.hideturtle()
 
-def Right():
-    while is_pressed("left"):
-        
-def Left():
-def Up():
-def Down():
+# Turning
+def turn_left():
+    bubble.left(90)
+
+
+def turn_right():
+    bubble.right(90)
+
+
+# Collection of the balls
+def collection(a, b):
+    return abs(a.xcor() - b.xcor()) < 10 and abs(a.ycor() - b.ycor()) < 20
+
+
+def collection_ball_restart():
+    collection_ball.color("black")
+    ball_cor1 = random.randint(30, 280)
+    ball_cor2 = random.randint(30, 280)
+    collection_ball.goto(ball_cor1, ball_cor2)
+    collection_ball.color("yellow")
+    bubble.forward(speed)
+    screen.ontimer(play_game, 10)
+
+
+def play_game():
+    if collection(bubble, collection_ball):
+        score = 0
+        score += 2
+        points.clear()
+        points.write("Points: " + str(score), font=style)
+        collection_ball_restart()
+        bubble.forward(speed)
+
+    else:
+        bubble.forward(speed)
+        screen.ontimer(play_game, 10)
+
+
+turtle.onkeypress(turn_left, "Left")
+turtle.onkeypress(turn_right, "Right")
+
+screen.listen()
+
+play_game()
 
 screen.mainloop()

eb82568b3eeb17d34b65cb07071d45ca1ea528ab
Cheșa David <140747134+ChesaDavid@users.noreply.github.com>
2024-01-06 16:25:48 +0200

    Save

--- screen.py
+++ screen.py
@@ -7,32 +7,49 @@ import random
 # pygame setup
 pygame.init()
 pygame.font.init()
-pygame.collidepoin
 
 screen = pygame.display.set_mode((1280, 720))
 
-score = 0
-score_increment = 10
+Redscore = 0
+Redscore_increment = 10
+
+BlueScore = 0
+BlueScore_increment = 10
 
 font = pygame.font.Font(None,32)
+
+redMase = 20
+blueMase = 20
+
 
 clock = pygame.time.Clock()
 running = True
 dt = 0
-obstacle = pygame.Rect(200, 200, 50, 50)
-bublle_collect = pygame.Rect(20,20,20,20)
+player2 = pygame.Rect(blueMase,blueMase,blueMase,blueMase)
+player1= pygame.Rect(redMase,redMase,redMase,redMase)
+
 
 
 
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
-obstacle_pos = pygame.Vector2(screen.get_width()/2+156,screen.get_height()/2+123)
+player2_pos = pygame.Vector2(screen.get_width()/2+156,screen.get_height()/2+123)
 
-def GameOver():
-    screen.fill("red")
-    pygame.draw.circle(screen, "red", player_pos, 0)
-    score_text = font.render(f'Score: {score}',True,(255,255,255))
-    screen.blit(score_text, (0, 0))
-    time.sleep(5)
+
+def collisions():
+    if redMase > blueMase:
+        score_text = font.render(f'Red win with a score of : {Redscore}',True,(255,255,255))
+        screen.blit(score_text, (screen.get_width() / 2, screen.get_height() / 2))
+    else:
+        score_text = font.render(f'Blue win with a score of : {BlueScore}',True,(255,255,255))
+        screen.blit(score_text, (screen.get_width() / 2, screen.get_height() / 2))  
+    time.sleep(4)   
+def collections():
+    if player1.collidepoint(prise):
+        redMase += Redscore_increment
+        Redscore += Redscore_increment
+    elif player2.collidepoint(prise):
+        blueMase += BlueScore_increment
+        BlueScore += BlueScore_increment
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
@@ -44,8 +61,8 @@ while running:
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("black")
 
-    pygame.draw.circle(screen, "red", player_pos, 40)
-    pygame.draw.circle(screen,"blue",obstacle_pos,20)
+    pygame.draw.circle(screen, "red", player_pos, redMase)
+    pygame.draw.circle(screen,"blue",player2_pos,blueMase)
     keys = pygame.key.get_pressed()
     if keys[pygame.K_UP]:
         player_pos.y -= 300 * dt
@@ -56,6 +73,16 @@ while running:
     if keys[pygame.K_RIGHT]:
         player_pos.x += 300 * dt
         
+        
+    
+    if keys[pygame.K_w]:
+        player2_pos.y -= 300 * dt
+    if keys[pygame.K_s]:
+        player2_pos.y += 300 * dt
+    if keys[pygame.K_a]:
+        player2_pos.x -= 300 * dt
+    if keys[pygame.K_d]:
+        player2_pos.x += 300 * dt
     
     if player_pos.x > 1280:
         running = False
@@ -69,10 +96,16 @@ while running:
         running = False
         
         
-    score_text = font.render(f'Score: {score}',True,(255,255,255))
+    score_text = font.render(f'Score red: {Redscore}',True,(255,255,255))
     screen.blit(score_text, (10, 10))
+    
+    score_text = font.render(f'Score blue: {BlueScore}',True,(255,255,255))
+    screen.blit(score_text, (10, 600))
 
     # flip() the display to put your work on screen
+    player2 = pygame.Rect(blueMase,blueMase,blueMase,blueMase)
+    player1= pygame.Rect(redMase,redMase,redMase,redMase)
+
     pygame.display.flip()
 
     # limits FPS to 60
